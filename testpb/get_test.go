083732c3560e4822091a28526dd3cf10759cc1c9
Frojdi Dymylja <frojdi.dymylja@gmail.com>
2021-07-22 18:30:49 +0200

    chore: add whichoneof test

--- testpb/get_test.go
+++ testpb/get_test.go
@@ -409,3 +409,16 @@ func TestGetOneof(t *testing.T) {
 		require.Equal(t, msg.GetONEOF_B(), mv.Interface())
 	})
 }
+
+func TestGetoneof(t *testing.T) {
+	x := &A{}
+	oneOf1 := x.ProtoReflect().Descriptor().Fields().ByName("ONEOF_STRING")
+	oneOf2 := x.ProtoReflect().Descriptor().Fields().ByName("ONEOF_B")
+	t.Logf("%s", oneOf1)
+
+	v := x.ProtoReflect().Get(oneOf1)
+	t.Logf("%s", v.Interface()) // empty string
+
+	v = x.ProtoReflect().Get(oneOf2)
+	t.Logf("%v", v.Message().IsValid()) // nil object, is valid
+}
