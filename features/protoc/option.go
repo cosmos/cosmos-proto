23b0bcab9183dc2aec572a8e7d5504403119eeef
technicallyty <48813565+tytech3@users.noreply.github.com>
2021-09-27 13:49:37 -0700

    feat: interfaces now supplied via configuration file

--- features/protoc/option.go
+++ features/protoc/option.go
@@ -1,31 +1,69 @@
 package protoc
 
-import "strings"
+import (
+	"gopkg.in/yaml.v3"
+	"io/ioutil"
+	"strings"
+)
 
-func extractOption(o string) (string, string) {
+
+type config struct {
+	Interfaces map[string]string `yaml:"interfaces"`
+}
+
+var cfg config
+
+func init() {
+	yfile, err := ioutil.ReadFile("config.yaml")
+	if err != nil {
+		panic(err)
+	}
+
+	err = yaml.Unmarshal(yfile, &cfg)
+	if err != nil {
+		panic(err)
+	}
+	//cfg.Interfaces = make(map[string]string)
+	//cfg.Interfaces["cosmos.gov.Content"] = "github.com/cosmos/cosmos-sdk/x/gov/types"
+	//cfg.Interfaces["cosmos.staking.Validator"] = "github.com/cosmos/cosmos-sdk/x/staking/types"
+}
+
+// cosmos proto option definitions
+var (
+	// field options
+	acceptsInterfaceFieldOption = "93001"
+	scalarFieldOption           = "93002"
+
+	// message options
+	implementsInterfaceMessageOption = "93001"
+)
+
+func unwrapOption(o string) (string, string) {
 	split := strings.Split(o, ":")
 	if len(split) != 2 {
 		panic("invalid option format")
 	}
-	return split[0], strings.TrimSpace(split[1])
+	return cleanString(split[0]), cleanString(split[1])
+}
+
+// for some reasons option strings have quotes and spaces diluting them
+// so we use this func to clean it up.
+func cleanString(s string) string {
+	s = strings.TrimSpace(s)
+	s = strings.Trim(s, "\"")
+	return s
 }
 
 func getIdentifier(n string) string {
 	// should take something like "cosmos.gov.Content"
 	// and simply return the identifier associated with it.
-	// in this case we want "Content"
-	return "Content"
-}
-
-func lookUpOption(n string) string {
-	// we should have some sort of yaml file or something that
-	// will map us from the field option number (i.e. 90021 or whatever) to
-	// "implements_interface" or "accepts_interface"
-	return "implements_interface"
+	// in the example above, this function would return "Content"
+	identifier := strings.Split(n, ".")
+	return identifier[len(identifier)-1]
 }
 
 // ideally this should look up whatever `o` is in a yaml config
 // i.e. cosmos.gov.Content: github.com/cosmos/cosmos-sdk/x/gov/types
 func getInterfaceImport(o string) string {
-	return "github.com/cosmos/cosmos-sdk/x/gov/types"
+	return cfg.Interfaces[o]
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,10 @@ module github.com/cosmos/cosmos-proto
 go 1.16
 
 require (
+	github.com/cosmos/cosmos-sdk v0.44.0
 	github.com/stretchr/testify v1.7.0
 	google.golang.org/protobuf v1.27.0
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b
 )
+
+replace github.com/gogo/protobuf => github.com/regen-network/protobuf v1.3.3-alpha.regen.1
