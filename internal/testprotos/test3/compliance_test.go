c9d25509182860f0d65996b307ca7816d04090df
Julien Robert <julien@rbrt.fr>
2024-02-12 15:37:04 +0100

    add failing test

--- internal/testprotos/test3/compliance_test.go
+++ internal/testprotos/test3/compliance_test.go
@@ -1,10 +1,21 @@
 package test3
 
 import (
-	"google.golang.org/protobuf/testing/prototest"
+	"strings"
 	"testing"
+
+	"google.golang.org/protobuf/testing/prototest"
 )
 
 func TestCompliance(t *testing.T) {
 	prototest.Message{}.Test(t, (&TestAllTypes{}).ProtoReflect().Type())
+}
+
+func TestSourceCodeInfo(t *testing.T) {
+	descriptor := (&ForeignMessage{}).ProtoReflect().Descriptor()
+	sourceInfos := descriptor.ParentFile().SourceLocations()
+
+	if !strings.Contains(sourceInfos.ByDescriptor(descriptor).LeadingComments, "This comment is for testing source code info comments") {
+		t.Errorf("LeadingComments not found in source info")
+	}
 }
